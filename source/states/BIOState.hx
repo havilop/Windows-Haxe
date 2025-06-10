50fd13928f69fc6eae90f90836555e30a6cf392c
havilop <ilyashokov066@gmail.com>
2025-06-10 15:59:15 +0300

    trying working on bios state

--- source/states/BIOState.hx
+++ source/states/BIOState.hx
@@ -1,11 +1,15 @@
 package states;
 
+import flixel.FlxSprite;
+import flixel.FlxBasic;
+import flixel.ui.FlxButton;
 import haxe.Timer;
 import haxe.Json;
 import sys.io.File;
 import openfl.net.FileReference;
 import sys.FileSystem;
 import openfl.Lib;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.FlxState;
 import flixel.FlxG;
@@ -24,9 +28,12 @@ class BIOState extends FlxState
     var o:BiosSettings;
     var IsBootReady:Bool;
     var TimeDelay:Int;
+    var bg:FlxSprite;
+    var itemsBios:FlxTypedGroup<FlxButton>;
+    var itemsBiosName:Array<String> = ["autombr","fastbios"];
     var IsBios:Bool = false;
 
-    public function AddStartUI() 
+    function AddStartUI() 
     {
         textMBR = new FlxText(10, FlxG.height - 135,0,"Press ESC to load MBR", 52);
         textMBR.setFormat(null, 52, 0xFFFFFF, "left");
@@ -36,9 +43,43 @@ class BIOState extends FlxState
         textBIOS.setFormat(null, 52, 0xFFFFFF, "left");
         add(textBIOS);
     }
-    public function AddBIOSUI() 
+    function AddBIOSUI() 
     {
-        
+        for (num => items in itemsBiosName)
+        {
+            trace(num);
+            var item:FlxButton = createBiosItem(items,0,(num * 135) + 250,function name() {
+                if (num == 0) 
+                {
+                    var first = false;
+
+                    if (first == false) 
+                    {
+                    var com = new CommandFunction("/autombr true");
+                    add(com);
+                    Timer.delay(function name() {
+                        first = true;
+                    },50);
+                    }
+                    if (first == true)
+                    {
+                    var com = new CommandFunction("/autombr false");
+                    add(com);
+                    first = false;
+                    }
+                }
+                if (num == 1)
+                {
+                    trace("1");
+                }
+            });
+        }
+    }
+    function createBiosItem(name:String,x:Float,y:Float,?void:Null<()-> Void>) 
+    {
+        var itemBios:FlxButton = new FlxButton(x,y,name,void);
+        itemsBios.add(itemBios);
+        return itemBios;
     }
     override function create() 
     {
@@ -52,7 +93,20 @@ class BIOState extends FlxState
         FlxG.autoPause = false;
 
         AddStartUI();
-        
+
+        bg = new FlxSprite(0,0,"assets/images/setup/bg.png");
+        bg.screenCenter(XY);
+        bg.setGraphicSize(FlxG.width,FlxG.height);
+        bg.color = 0x0400ff;
+        bg.visible = false;
+        add(bg);
+
+        itemsBios = new FlxTypedGroup<FlxButton>();
+        itemsBios.visible = false;
+		add(itemsBios);
+
+        AddBIOSUI();
+
         if (FileSystem.exists("assets/data/settings.json")) 
         {
             var data = File.getContent("assets/data/settings.json");
@@ -101,6 +155,27 @@ class BIOState extends FlxState
             {
                 LoadState.setLoadingScreen(1,MBRstate.new);
             }
+        }
+        if (FlxG.keys.justPressed.DELETE)
+        {
+            IsBios = true;
+        }
+        if (IsBios == true)
+        {
+            itemsBios.visible = true;
+            FlxG.mouse.visible = true;
+            bg.visible = true;
+
+            if (FlxG.keys.justReleased.ESCAPE)
+            {
+                IsBios = false;
+            }
+        }
+        if (IsBios == false)
+        {
+             itemsBios.visible = false;
+            FlxG.mouse.visible = false;
+            bg.visible = false;
         }
     }
 }
